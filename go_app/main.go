cfd4d132aac46858a9dec674b1a5968abf654d72
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-05-04 17:11:04 +0000

    finished go

---
+++ go_app/main.go
@@ -0,0 +1,38 @@
+// Created by: Dominic M.
+// Created on: April 2023
+//
+// This program checks to see if you a eligible for student pricing.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var day string
+	var age int
+
+	// input
+	fmt.Println("This program checks to see if you a eligible for student pricing.")
+	fmt.Println()
+	fmt.Print("Day is it? ")
+	fmt.Println()
+	fmt.Scanln(&day)
+	fmt.Println()
+	fmt.Print("How old are you? ")
+	fmt.Println()
+	fmt.Scanln(&age)
+
+	// process
+	if day == "tuesday" || day == "thursday" || day == "Tuesday" || day == "Thursday" || age > 12 && age < 21 {
+		fmt.Println("You are eligible for student pricing.")
+		fmt.Println()
+		fmt.Print("\nDone.")
+	} else {
+		fmt.Println("You are not eligible for student pricing.")
+		fmt.Println()
+		fmt.Print("\nDone.")
+
+	}
+}
