6a517a7493a4e2f0066f90d71a7c02a9d0fae2ea
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-05-04 16:54:21 +0000

    finished start

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Dominic M. All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dominic M.
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+/**
+ * This function uses a selection component from https://github.com/CreativeIT/getmdl-select
+ */
+
+function myButtonClicked() {
+  // input
+  const age = parseInt(document.getElementById('age-entered').value)
+  const day = document.getElementById('day-selected').value
+
+
+
+  // process
+  if (day == "Tuesday" || day == "Thursday" || age > 12 && age < 21) {
+  document.getElementById('answer').innerHTML = "You get the student discount."
+  } else {
+    document.getElementById('answer').innerHTML = "You must pay regular price."
+  }
+}
